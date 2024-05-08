8a67ab6c8972af6723153af68dc9254abebab591
Moonofweisheng <1780903673@qq.com>
2024-05-08 05:35:58 +0000

    Deploying to gh-pages from @ Moonofweisheng/wot-design-uni@1314373af1dff95aee4cd3cddefebcb3a6a5c5d2 🚀

---
+++ demo/assets/useTranslate.23fa9f56.js
@@ -0,0 +1,1 @@
+import{D as s,L as a,E as r,F as t}from"./page-wraper.84820f31.js";const e=e=>{const n=e?s(e)+".":"";return{translate:(s,...e)=>{const o=a.messages(),p=r(o,n+s);return t(p)?p(...e):p}}};export{e as u};
